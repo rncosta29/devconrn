03b7631c7ee6542dda0b088a138b3e010761eca9
Rafael Costa <raf.costa29@gmail.com>
2022-06-27 16:54:02 -0300

    Primeiro commit

--- App.js
+++ App.js
@@ -1,20 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+import { StateProvider } from './src/contexts/StateContext';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import AuthStack from './src/stacks/AuthStack';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default () => {
+    return(
+        <StateProvider>
+            <NavigationContainer>
+                <AuthStack />
+            </NavigationContainer>
+        </StateProvider>
+    );
+}  
---
+++ src/screens/PreloadScreen/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import C from './style';
+
+export default () => {
+    return(
+        <C.Container>
+            <C.LoadingIcon color="#8863E6" size="large" />
+        </C.Container>
+    )
+}
